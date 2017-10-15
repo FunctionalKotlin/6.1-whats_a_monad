d6d222eca5c0ca555fb07c674d6de09711dbc6a9
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-15 20:14:23 +0200

    Burn The Flatten!!

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -11,12 +11,10 @@ fun <A, B> Option<A>.map(transform: (A) -> B): Option<B> = when(this) {
     is Just -> Just(transform(value))
 }
 
-fun <A> Option<Option<A>>.flatten(): Option<A> = when(this) {
+fun <A, B> Option<A>.flatMap(transform: (A) -> Option<B>): Option<B> = when(this) {
     is None -> None
-    is Just -> this.value
+    is Just -> transform(this.value)
 }
-
-fun <A, B> Option<A>.flatMap(transform: (A) -> Option<B>): Option<B> = this.map(transform).flatten()
 
 fun <A> Option<A>.ifPresent(execute: (A) -> Unit) {
     if (this is Just) execute(this.value)
