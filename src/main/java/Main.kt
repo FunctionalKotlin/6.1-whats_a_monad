918378ab6c816419da42b36596550ca3da29060c
Alejandro Hernández <info@alejandrohdezma.com>
2017-10-15 19:53:58 +0200

    The Flatten

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -11,6 +11,11 @@ fun <A, B> Option<A>.map(transform: (A) -> B): Option<B> = when(this) {
     is Just -> Just(transform(value))
 }
 
+fun <A> Option<Option<A>>.flatten(): Option<A> = when(this) {
+    is None -> None
+    is Just -> this.value
+}
+
 fun <A> Option<A>.ifPresent(execute: (A) -> Unit) {
     if (this is Just) execute(this.value)
 }
@@ -28,8 +33,9 @@ fun getOrganizationById(organizationId: Int): Option<Organization> =
     Just(Organization("FunctionalHub"))
 
 fun main(args: Array<String>) {
-    val organization: Option<Option<Organization>> = getUserById(42)
+    val organization: Option<Organization> = getUserById(42)
         .map { user -> getOrganizationById(user.organizationId) }
+        .flatten()
 
     organization.ifPresent { println(it) }
 }
